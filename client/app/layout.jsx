d974bdc80f175f343fb713d851a886e7a3ee9735
Somkene Ojukwu <somkeneoj@gmail.com>
2024-02-03 13:39:41 +0100

    new font

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -16,9 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"></link>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+<link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet"></link>
         <title>Retweet</title>
       </head>
       <body className='body'>
